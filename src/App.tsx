cecde01b99f9c40cf9e52eb8764d8f9387b3a684
Rajkumar Veeramalli <veeramalli.rajkumar@cbre.com>
2025-11-25 14:56:36 +0530

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import EmeraldLogo from "./assets/emerald-logo.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,8 +16,15 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://docs.emerald.cbre.com/" target="_blank">
+          <img
+            src={EmeraldLogo}
+            className="logo emerald"
+            alt="Emerald Design System logo"
+          />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + EDS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
